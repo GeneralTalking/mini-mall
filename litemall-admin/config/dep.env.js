1340431883c9a99c654e3fd4d0dd7cc3c02b3cd8
xujiquan <xujiquan@juqitech.com>
2018-05-15 10:11:29 +0800

    修正小程序登陆失败的问题

--- litemall-admin/config/dep.env.js
+++ litemall-admin/config/dep.env.js
@@ -1,6 +1,6 @@
 module.exports = {
 	NODE_ENV: '"deploy"',
 	ENV_CONFIG: '"dep"',
-    BASE_API: '"http://122.152.206.172:8083/admin"',
-    OS_API: '"http://122.152.206.172:8081/os"'
+    BASE_API: '"http://www.941fx.cn/admin-api"',
+    OS_API: '"http://www.941fx.cn/os-api"'
 }
